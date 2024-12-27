fb18222fa25e9e341c8e438d73082f09f96d81d2
i3po <i3poac@gmail.com>
2024-12-27 21:02:08 +0300

    0.4.2

--- app/src/main/java/com/myAllVideoBrowser/util/FileUtil.kt
+++ app/src/main/java/com/myAllVideoBrowser/util/FileUtil.kt
@@ -5,9 +5,6 @@ import android.content.ContentResolver
 import android.content.ContentUris
 import android.content.ContentValues
 import android.content.Context
-import android.content.Intent
-import android.graphics.Bitmap
-import android.media.MediaMetadataRetriever
 import android.net.Uri
 import android.os.Build
 import android.os.Environment

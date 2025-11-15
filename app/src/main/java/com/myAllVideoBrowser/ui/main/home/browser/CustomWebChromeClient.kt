7adc4d9a3c5a29a4be8aae373fd7f594da383b5a
i3po <i3poac@gmail.com>
2025-11-15 09:13:06 +0300

    drm permissions removed

--- app/src/main/java/com/myAllVideoBrowser/ui/main/home/browser/CustomWebChromeClient.kt
+++ app/src/main/java/com/myAllVideoBrowser/ui/main/home/browser/CustomWebChromeClient.kt
@@ -27,19 +27,6 @@ class CustomWebChromeClient(
     private val appUtil: AppUtil,
     private val mainActivity: MainActivity
 ) : WebChromeClient() {
-    override fun onPermissionRequest(request: PermissionRequest?) {
-        val resources = request?.resources
-        resources?.forEach { resource ->
-            if (
-                PermissionRequest.RESOURCE_PROTECTED_MEDIA_ID == resource
-            ) {
-                request.grant(resources)
-                return
-            }
-        }
-        super.onPermissionRequest(request)
-    }
-
     override fun onCreateWindow(
         view: WebView?,
         isDialog: Boolean,

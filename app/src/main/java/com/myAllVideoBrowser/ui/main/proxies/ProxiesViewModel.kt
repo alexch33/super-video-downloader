fdc2046d72340e9fa1985238743b265124388356
i3po <i3poac@gmail.com>
2025-12-22 17:49:30 +0300

    added dns over https support to UI && refactoring

--- app/src/main/java/com/myAllVideoBrowser/ui/main/proxies/ProxiesViewModel.kt
+++ app/src/main/java/com/myAllVideoBrowser/ui/main/proxies/ProxiesViewModel.kt
@@ -21,15 +21,49 @@ class ProxiesViewModel @Inject constructor(
 
     val isProxyOn = ObservableField(false)
 
-    private val defaultDohConfig = "doh=strict:https://cloudflare-dns.com/dns-query"
+    enum class DohProvider(val url: String) {
+        CLOUDFLARE("doh=strict:https://cloudflare-dns.com/dns-query"), QUAD9("doh=strict:https://dns.quad9.net/dns-query"), ADGUARD(
+            "doh=strict:https://dns.adguard-dns.com/dns-query"
+        )
+    }
+
+    val isDohOn = ObservableField(true)
+
+    val selectedDohProvider = ObservableField(DohProvider.CLOUDFLARE)
 
     override fun start() {
         viewModelScope.launch(Dispatchers.IO) {
+            isDohOn.set(sharedPrefHelper.getIsDohOn())
+            val savedProviderName = sharedPrefHelper.getSelectedDohProvider()
+            val provider =
+                DohProvider.entries.find { it.name == savedProviderName } ?: DohProvider.CLOUDFLARE
+            selectedDohProvider.set(provider)
+
             val usersProxy = sharedPrefHelper.getUserProxyChain()
             proxiesList.set(usersProxy.toMutableList())
             realProxyCount.set(usersProxy.count { it != Proxy.noProxy() })
-            isProxyOn.set(proxyController.isProxyOn())
+            isProxyOn.set(sharedPrefHelper.getIsProxyOn())
             updateChain(usersProxy.toList())
+        }
+    }
+
+    fun setDohState(isOn: Boolean) {
+        viewModelScope.launch(Dispatchers.IO) {
+            isDohOn.set(isOn)
+            sharedPrefHelper.setIsDohOn(isOn)
+            proxyController.updateProxyState()
+            updateChain(proxiesList.get() ?: emptyList())
+        }
+    }
+
+    fun setDohProvider(provider: DohProvider) {
+        viewModelScope.launch(Dispatchers.IO) {
+            selectedDohProvider.set(provider)
+            sharedPrefHelper.saveSelectedDohProvider(provider.name)
+            proxyController.updateProxyState()
+            if (isDohOn.get() == true) {
+                updateChain(proxiesList.get() ?: emptyList())
+            }
         }
     }
 
@@ -49,26 +83,10 @@ class ProxiesViewModel @Inject constructor(
         }
     }
 
-    private fun saveAndUpdateProxies(updatedList: List<Proxy>) {
-        val cleanedList = if (updatedList.size > 1) {
-            updatedList.filter { it != Proxy.noProxy() }
-        } else {
-            updatedList
-        }
-
-        sharedPrefHelper.saveUserProxyChain(cleanedList.toTypedArray())
-
-        proxiesList.set(cleanedList.toMutableList())
-        realProxyCount.set(cleanedList.count { it != Proxy.noProxy() })
-
-        if (isProxyOn.get() == true) {
-            updateChain(cleanedList)
-        }
-    }
-
     fun turnOffProxy() {
         viewModelScope.launch(Dispatchers.IO) {
-            proxyController.setIsProxyOn(false)
+            sharedPrefHelper.setIsProxyOn(false)
+            proxyController.updateProxyState()
             isProxyOn.set(false)
             updateChain(listOf(Proxy.noProxy()))
         }
@@ -76,27 +94,46 @@ class ProxiesViewModel @Inject constructor(
 
     fun turnOnProxy() {
         viewModelScope.launch(Dispatchers.IO) {
-            proxyController.setIsProxyOn(true)
+            sharedPrefHelper.setIsProxyOn(true)
+            proxyController.updateProxyState()
             isProxyOn.set(true)
             updateChain(sharedPrefHelper.getUserProxyChain().toList())
         }
     }
 
     private fun updateChain(proxies: List<Proxy>) {
-        val proxy = proxies.firstOrNull() ?: Proxy.noProxy()
-        if (proxy != Proxy.noProxy()) {
-            val formattedProxyChain = proxies.map { it.toString() }
-            ProxyManager.updateChain(
-                formattedProxyChain + listOf(
-                    defaultDohConfig
-                )
-            )
-        } else {
-            ProxyManager.updateChain(
-                listOf(
-                    defaultDohConfig
-                )
-            )
+        val proxyChain = mutableListOf<String>()
+        val realProxies = proxies.filter { it != Proxy.noProxy() }
+
+        if (realProxies.isNotEmpty()) {
+            proxyChain.addAll(realProxies.map { it.toString() })
+        }
+
+        if (isDohOn.get() == true) {
+            selectedDohProvider.get()?.let { provider ->
+                proxyChain.add(provider.url)
+            }
+        }
+
+        ProxyManager.updateChain(proxyChain)
+    }
+
+    private fun saveAndUpdateProxies(updatedList: List<Proxy>) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val cleanedList = if (updatedList.size > 1) {
+                updatedList.filter { it != Proxy.noProxy() }
+            } else {
+                updatedList
+            }
+
+            sharedPrefHelper.saveUserProxyChain(cleanedList.toTypedArray())
+
+            proxiesList.set(cleanedList.toMutableList())
+            realProxyCount.set(cleanedList.count { it != Proxy.noProxy() })
+            proxyController.updateProxyState()
+            if (isProxyOn.get() == true) {
+                updateChain(cleanedList)
+            }
         }
     }
 

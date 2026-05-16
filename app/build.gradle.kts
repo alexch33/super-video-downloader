815fa9135af17f4e233b484686a94bbe0bffa5a1
i3po <i3poac@gmail.com>
2026-05-16 09:33:54 +0300

    prerelease 0.8.17.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -115,8 +115,8 @@ android {
         applicationId = "com.myAllVideoBrowser"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 228
-        versionName = "0.8.17"
+        versionCode = 234
+        versionName = "0.8.17.1"
 
         if (splitApks) {
             splits {

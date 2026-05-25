a04b68d7e6c096b7d2932ba56f227714f027ce4e
i3po <i3poac@gmail.com>
2026-05-25 05:15:40 +0300

    throw memory error from okhttp interceptor

--- app/src/main/java/com/myAllVideoBrowser/util/ProxyRetryInterceptor.kt
+++ app/src/main/java/com/myAllVideoBrowser/util/ProxyRetryInterceptor.kt
@@ -1,14 +1,19 @@
 package com.myAllVideoBrowser.util
 
+import android.content.Context
 import okhttp3.Interceptor
 import okhttp3.Response
 
 
-class ProxyRetryInterceptor : Interceptor {
+class ProxyRetryInterceptor(private val context: Context) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
         var attempt = 0
         val maxTries = 3
+
+        if (Memory.isMemoryCritical(context)) {
+            throw Error("OOM error")
+        }
 
         while (true) {
             try {

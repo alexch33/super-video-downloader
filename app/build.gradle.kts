a56078ba06d9a4160f60070425400d6ceed9ab36
i3po <i3poac@gmail.com>
2026-06-04 17:58:06 +0300

    ci rust cache fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -115,8 +115,8 @@ android {
         applicationId = "com.myAllVideoBrowser"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 298
-        versionName = "0.8.20.2"
+        versionCode = 302
+        versionName = "0.8.20.3"
 
         if (splitApks) {
             splits {
